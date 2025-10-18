2a5faabb96b75e202fe2f00a789eba750b1d582e
ironballista <87903825+ironballista@users.noreply.github.com>
2025-10-18 21:24:48 +0200

    Initial commit.

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Nix Docker Layered Image test.";
+
+  # Nixpkgs / NixOS version to use.
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    naersk.url = "github:nix-community/naersk";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, naersk, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = (import nixpkgs) {
+            inherit system;
+          };
+
+          naersk' = pkgs.callPackage naersk {};
+
+        in rec {
+
+          packages = {
+            app = naersk'.buildPackage {
+              src = ./.;
+            };
+            default = packages.app;
+            docker = pkgs.dockerTools.buildLayeredImage {
+              name = "localhost/nix-test";
+              tag = "latest";
+              contents = [ packages.default ];
+              config.Cmd = "${packages.default}/bin/nix-docker-test";
+            };
+          };
+
+          # For `nix develop` (optional, can be skipped):
+          devShell = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [ rustc cargo jujutsu ];
+          };
+        }
+      );
+}
